eb0f7ffb4cf27e691a2123c5eb06ebfbc31cf707
joey <lvliangxiong@foxmail.com>
2021-01-22 14:41:11 +0800

    improve the program safety and tips for easy start

--- VoiceReversal.m
+++ VoiceReversal.m
@@ -2,13 +2,14 @@ classdef VoiceReversal < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        VoiceReversalUIFigure  matlab.ui.Figure
-        GridLayout             matlab.ui.container.GridLayout
-        TextArea               matlab.ui.control.TextArea
-        PlayButton             matlab.ui.control.Button
-        ReverseButton          matlab.ui.control.Button
-        StopButton             matlab.ui.control.Button
-        StartButton            matlab.ui.control.Button
+        VoiceReversalUIFigure      matlab.ui.Figure
+        GridLayout                 matlab.ui.container.GridLayout
+        TextArea                   matlab.ui.control.TextArea
+        PlayButton                 matlab.ui.control.Button
+        ReverseButton              matlab.ui.control.Button
+        StopButton                 matlab.ui.control.Button
+        StartButton                matlab.ui.control.Button
+        DesignedbyJoeyEvelynLabel  matlab.ui.control.Label
     end
 
     
@@ -16,12 +17,19 @@ classdef VoiceReversal < matlab.apps.AppBase
         Fs = 48000
         nBits = 16
         nChannels = 2
-        id = -1 % default audio input device 
+        id = -1 % default audio input device
+        gain = 25 % Description
     end
 
     properties (Access = private)
         recorder audiorecorder
         audioData = -1 % Description
+    end
+    
+    methods (Access = private)
+        function func(app)
+            app.TextArea.Value = "Still recording ...";
+        end
     end
     
 
@@ -31,32 +39,47 @@ classdef VoiceReversal < matlab.apps.AppBase
         % Code that executes after component creation
         function startupFcn(app)
             app.recorder = audiorecorder(app.Fs, app.nBits, app.nChannels, app.id);
+            app.recorder.TimerPeriod = 1; % seconds
+            app.recorder.TimerFcn = @(recorder,event) func(app);
         end
 
         % Button pushed function: StartButton
         function StartButtonPushed(app, event)
-            app.TextArea.Value = "Recording!";
+            if isrecording(app.recorder)
+                stop(app.recorder);
+            end
             record(app.recorder);
+            app.TextArea.Value = "Recording!";
         end
 
         % Button pushed function: StopButton
         function StopButtonPushed(app, event)
             if isrecording(app.recorder)
                 stop(app.recorder);
+                app.TextArea.Value = 'Record stopped!';
+                app.audioData = getaudiodata(app.recorder) * app.gain; % 注意这是一个列向量
+            else
+                app.TextArea.Value = 'Pls start recording first!';
             end
-            app.TextArea.Value = 'Record stopped!';
-            app.audioData = getaudiodata(app.recorder); % 注意这是一个列向量
         end
 
         % Button pushed function: ReverseButton
         function ReverseButtonPushed(app, event)
-            app.audioData = flip(app.audioData);
-            app.TextArea.Value = 'Voice reversed!';
+            if app.audioData ~= -1
+                app.audioData = flip(app.audioData);
+                app.TextArea.Value = 'Voice reversed!';
+            else
+                app.TextArea.Value = 'No voice recorded, pls record first!';
+            end
         end
 
         % Button pushed function: PlayButton
         function PlayButtonPushed(app, event)
-            sound(app.audioData, app.recorder.get('SampleRate'), app.recorder.get('BitsPerSample'));
+            if app.audioData ~= -1
+                sound(app.audioData, app.recorder.get('SampleRate'), app.recorder.get('BitsPerSample'));
+            else
+                app.TextArea.Value = 'No voice recorded, pls record first!';
+            end
         end
     end
 
@@ -69,14 +92,14 @@ classdef VoiceReversal < matlab.apps.AppBase
             % Create VoiceReversalUIFigure and hide until all components are created
             app.VoiceReversalUIFigure = uifigure('Visible', 'off');
             app.VoiceReversalUIFigure.AutoResizeChildren = 'off';
-            app.VoiceReversalUIFigure.Position = [100 100 246 124];
+            app.VoiceReversalUIFigure.Position = [500 300 250 150];
             app.VoiceReversalUIFigure.Name = 'VoiceReversal';
             app.VoiceReversalUIFigure.Resize = 'off';
 
             % Create GridLayout
             app.GridLayout = uigridlayout(app.VoiceReversalUIFigure);
             app.GridLayout.ColumnWidth = {64, 64, 64};
-            app.GridLayout.RowHeight = {26, 26, 26, '1x', '2.03x'};
+            app.GridLayout.RowHeight = {26, 26, 26, '1x'};
             app.GridLayout.ColumnSpacing = 9.25;
             app.GridLayout.Padding = [9.25 10 9.25 10];
 
@@ -112,6 +135,16 @@ classdef VoiceReversal < matlab.apps.AppBase
             app.StartButton.Layout.Row = 1;
             app.StartButton.Layout.Column = 1;
             app.StartButton.Text = 'Start';
+
+            % Create DesignedbyJoeyEvelynLabel
+            app.DesignedbyJoeyEvelynLabel = uilabel(app.GridLayout);
+            app.DesignedbyJoeyEvelynLabel.HorizontalAlignment = 'right';
+            app.DesignedbyJoeyEvelynLabel.VerticalAlignment = 'top';
+            app.DesignedbyJoeyEvelynLabel.FontSize = 11;
+            app.DesignedbyJoeyEvelynLabel.FontAngle = 'italic';
+            app.DesignedbyJoeyEvelynLabel.Layout.Row = 4;
+            app.DesignedbyJoeyEvelynLabel.Layout.Column = [1 3];
+            app.DesignedbyJoeyEvelynLabel.Text = 'Designed by Joey & Evelyn';
 
             % Show the figure after all components are created
             app.VoiceReversalUIFigure.Visible = 'on';
